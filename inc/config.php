a124aefc7adc510b79fe50d54905fd2e6cbbc124
ricardomb94 <aymardmb@gmail.com>
2020-10-23 00:56:23 +0200

    Add database sql setup

--- inc/config.php
+++ inc/config.php
@@ -1,6 +1,19 @@
 <?php
 
-  if(!defined('__CONFIG__')){
-    exit('You do not have a config file');
-  }
+if(!defined('__CONFIG__')){
+  exit('You do not have a config file');
+}
+
+
+//
+
+
+//Include the DB.php file
+include_once "classes/DB.php";
+
+$con = DB::getConnection();
+
+
+
+
 ?>
--- inc/footer.php
+++ inc/footer.php
@@ -6,3 +6,4 @@
 <!-- UIkit JS -->
 <script src="https://cdn.jsdelivr.net/npm/uikit@3.5.9/dist/js/uikit.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/uikit@3.5.9/dist/js/uikit-icons.min.js"></script>
+<!-- <script src="/assets/js/main.js"></script> -->
